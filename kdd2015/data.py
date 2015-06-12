5f50b882dd3754b080d02f07a4642a9b36851c0d
floydsoft <floydsoft@gmail.com>
2015-06-12 18:09:43 +0800

    add date column

--- kdd2015/data.py
+++ kdd2015/data.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import os.path
 
 from pandas import MultiIndex, DataFrame
+from pandas.tseries.offsets import DateOffset
 import pandas as pd
 import numpy as np
 # from numpy import datetime64
@@ -29,11 +30,11 @@ def load_csv():
 
     log_df = pd.concat([log_train, log_test], ignore_index=True)
     log_df = pd.merge(log_df, truth_df, how='left', on='enrollment_id')
-    enrollment_df = pd.concat(
+    raw_enrollment_df = pd.concat(
         [enrollment_train, enrollment_test], ignore_index=True)
-    log_df = pd.merge(log_df, enrollment_df, how='left', on='enrollment_id')
+    log_df = pd.merge(log_df, raw_enrollment_df, how='left', on='enrollment_id')
 
-    del enrollment_df
+    raw_enrollment_df.set_index('enrollment_id', inplace=True)
 
     log_df['date'] = log_df['time'].map(pd.datetools.normalize_date)
 
@@ -44,7 +45,7 @@ def load_csv():
     log_df['course_id'] = log_df['course_id'].astype('category')
 
     selected_indices = ['enrollment_id', 'username', 'course_id', 'date']
-    log_df = log_df.set_index(selected_indices)
+    log_df.set_index(selected_indices, inplace=True)
 
     log_df.sortlevel(inplace=True)
 
@@ -52,13 +53,23 @@ def load_csv():
 
     start_dates = course_groups['time'].agg(np.min).map(
         pd.datetools.normalize_date)
-    start_dates.name = 'start_dates'
+    start_dates.name = 'start_date'
     start_dates = DataFrame(start_dates)
 
     end_dates = course_groups['time'].agg(np.max).map(
         pd.datetools.normalize_date)
-    end_dates.name = 'end_dates'
+    end_dates.name = 'end_date'
     end_dates = DataFrame(end_dates)
+
+    course_ids = course_groups.groups.keys()
+    course_df = DataFrame({'course_id': list(course_ids)})
+
+    course_df.set_index('course_id', inplace=True)
+
+    course_df = course_df.merge(
+        start_dates, left_index=True, right_index=True)
+    course_df = course_df.merge(
+        end_dates, left_index=True, right_index=True)
 
     log_df['start_date'] = start_dates.reindex(
         log_df.index, level='course_id')
@@ -74,20 +85,49 @@ def load_csv():
         names=['enrollment_id', 'day'])
 
     enrollment_df = DataFrame(index=index)
-    enrollment_df = append_features(enrollment_df, log_df)
+    enrollment_df = enrollment_df.join(raw_enrollment_df)
 
-    log_df.to_pickle('log_df.pickle')
-    truth_df.to_pickle('truth_df.pickle')
+    def cal_date(index):
+        if not hasattr(cal_date, '_start_dates'):
+            cal_date._start_dates = {}
+
+        if not hasattr(cal_date, '_date_offsets'):
+            cal_date._date_offsets = [DateOffset(i) for i in range(30)]
+
+        if not hasattr(cal_date, '_course_ids'):
+            cal_date._course_ids = {}
+
+        if index[0] not in cal_date._course_ids:
+            cal_date._course_ids[index[0]] = \
+                enrollment_df.loc[index, 'course_id']
+
+        course_id = cal_date._course_ids[index[0]]
+
+        if course_id not in cal_date._start_dates:
+            cal_date._start_dates[course_id] = \
+                course_df.loc[course_id]['start_date']
+
+        start_date = cal_date._start_dates[course_id]
+        return start_date + cal_date._date_offsets[index[1]]
+
+    enrollment_df['date'] = enrollment_df.index.map(cal_date)
+
+    enrollment_df = append_features(enrollment_df, log_df, course_df)
+
     enrollment_df.to_pickle('enrollment_df.pickle')
+    truth_df.to_pickle('truth_df.pickle')
+    log_df.to_pickle('log_df.pickle')
+    course_df.to_pickle('course_df.pickle')
 
-    return log_df, truth_df, enrollment_df
+    return enrollment_df, truth_df, log_df, course_df
 
 
 def load_pickle():
-    log_df = pd.read_pickle('log_df.pickle')
-    truth_df = pd.read_pickle('truth_df.pickle')
     enrollment_df = pd.read_pickle('enrollment_df.pickle')
-    return log_df, truth_df, enrollment_df
+    truth_df = pd.read_pickle('truth_df.pickle')
+    log_df = pd.read_pickle('log_df.pickle')
+    course_df = pd.read_pickle('course_df.pickle')
+    return enrollment_df, truth_df, log_df, course_df
 
 
 def to_submission(result):
@@ -104,9 +144,9 @@ def load_data():
     if not (os.path.isfile('log_df.pickle') or
             os.path.isfile('truth_df.pickle') or
             os.path.isfile('enrollment_df.pickle')):
-        log_df, truth_df, enrollment_df = load_csv()
+        enrollment_df, truth_df, log_df, course_df = load_csv()
     else:
-        log_df, truth_df, enrollment_df = load_pickle()
+        enrollment_df, truth_df, log_df, course_df = load_pickle()
 
     if not os.path.isfile('data.h5'):
         x_train, y_train, x_test = df2array(log_df)
