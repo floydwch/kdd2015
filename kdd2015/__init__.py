e14cef506760ec483c35e8d63daeb04fa556b1de
floydwch <floydwch@gmail.com>
2017-01-12 12:10:13 +0800

    push all final updates

--- kdd2015/__init__.py
+++ kdd2015/__init__.py
@@ -8,3 +8,8 @@ gc.disable()
 pd.set_option('display.max_colwidth', 50)
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.width', None)
+
+setting = {
+    'multiprocessing': False,
+    'njob': 32
+}
